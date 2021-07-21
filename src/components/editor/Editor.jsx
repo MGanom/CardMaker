84ee5914c8ae349845946a29b09e95cffbfb4bb4
MGanom <chewy111@naver.com>
2021-07-21 19:36:57 +0900

    add card editor form

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import CardEdit from '../cardedit/CardEdit';
 import styles from './Editor.module.css';
 
-const Editor = props => (
+const Editor = ({ cards }) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Editor</h1>
+    {cards.map(card => (
+      <CardEdit card={card} />
+    ))}
   </section>
 );
 
--- src/components/maker/Maker.jsx
+++ src/components/maker/Maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/Editor';
 import Footer from '../footer/Footer';
@@ -7,6 +7,42 @@ import Preview from '../preview/Preview';
 import styles from './Maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Moon',
+      company: 'MoonCorp',
+      theme: 'light',
+      title: 'FrontEnd Developer',
+      email: 'chewy111@naver.com',
+      message: 'Code for Horde',
+      fileName: 'none',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'MoonGa',
+      company: 'MoonCorp',
+      theme: 'colorful',
+      title: 'FrontEnd Developer',
+      email: 'chewy111@naver.com',
+      message: 'Code for Horde',
+      fileName: 'none',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'MoonGanom',
+      company: 'MoonCorp',
+      theme: 'dark',
+      title: 'FrontEnd Developer',
+      email: 'chewy111@naver.com',
+      message: 'Code for Horde',
+      fileName: 'none',
+      fileURL: null,
+    },
+  ]);
+
   const history = useHistory();
 
   const onLogout = () => {
@@ -25,8 +61,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
